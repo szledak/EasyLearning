823b49de6c2bfcedf65a35b859476abb8ce2e1f5
Sonia Szledak <szledak@op.pl>
2015-06-17 11:48:34 +0200

    add data to QTableWidget

--- editdialog.cpp
+++ editdialog.cpp
@@ -1,6 +1,7 @@
 #include "editdialog.h"
 #include "ui_editdialog.h"
 #include <QDebug>
+#include <QMessageBox>
 
 EditDialog::EditDialog(QString fName, QWidget *parent) :
     QDialog(parent),
@@ -8,11 +9,41 @@ EditDialog::EditDialog(QString fName, QWidget *parent) :
 {
     filename = fName;
     ui->setupUi(this);
-
     this->setWindowTitle(filename);
+
+    QStringList list;
+    list << "Site A" << "Site B";
+    ui->tableWidget->setColumnCount(2);
+    ui->tableWidget->setHorizontalHeaderLabels(list);
+
+    //    int place = ui->tableWidget->rowCount() - 1;
+
+
 }
 
 EditDialog::~EditDialog()
 {
     delete ui;
+}
+
+void EditDialog::on_btnAdd_clicked()
+{
+    QString siteA = ui->edtSiteA->text();
+    QString siteB = ui->edtSiteB->text();
+
+    if(siteA.isEmpty() || siteB.isEmpty())
+        QMessageBox::warning(NULL, "Empty filename", "You did not enter a word!", "OK");
+    else
+    {
+        vector.append(siteA);
+        vector.append(siteB);
+
+        ui->edtSiteA->clear();
+        ui->edtSiteB->clear();
+
+        ui->tableWidget->insertRow(ui->tableWidget->rowCount());
+
+        ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 0, new QTableWidgetItem(siteA));
+        ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 1, new QTableWidgetItem(siteB));
+    }
 }
--- editdialog.h
+++ editdialog.h
@@ -15,9 +15,13 @@ public:
     explicit EditDialog(QString fName, QWidget *parent = 0);
     ~EditDialog();
 
+private slots:
+    void on_btnAdd_clicked();
+
 private:
     Ui::EditDialog *ui;
     QString filename;
+    QString vector;
 };
 
 #endif // EDITDIALOG_H
