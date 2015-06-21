a5dfca9e63a86bcca532f489b136868a35f86ae5
Sonia Szledak <szledak@op.pl>
2015-06-21 12:39:32 +0200

    Remove maximize window button

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowFlags( Qt::WindowTitleHint |  Qt::WindowCloseButtonHint |Qt::WindowMinimizeButtonHint | Qt::WindowSystemMenuHint);
+
     w.show();
 
     return a.exec();
